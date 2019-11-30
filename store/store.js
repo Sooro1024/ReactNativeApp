5014e3c90f918877a75922ca57ea60921e939dda
sooro1024 <soorogorgoyan@gmail.com>
2019-11-30 04:09:01 +0400

    delete logger frome store

--- store/store.js
+++ store/store.js
@@ -2,18 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import ThunckMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 
-const logger = store => nextSore => action => {
-  console.log({ action });
-  nextSore(action);
-};
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(ThunckMiddleware, logger)
-);
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
+const store = createStore(rootReducer, applyMiddleware(ThunckMiddleware));
 
 export default store;
